3fc3c3d7936e501a84d6e1cbf9d491200e4673df
joaohugo89 <joaohugosoares89@outlook.com>
2025-02-01 16:04:19 -0300

    Desenvolvimento API Cardapio - Banco de Dados Postgresql (foods)

---
+++ cardapio/src/main/java/com/hugo/cardapio/controller/FoodController.java
@@ -0,0 +1,42 @@
+package com.hugo.cardapio.controller;
+import com.hugo.cardapio.food.Food;
+import com.hugo.cardapio.food.FoodRepository;
+import com.hugo.cardapio.food.FoodRequestDTO;
+import com.hugo.cardapio.food.FoodResponseDTO;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+    
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping    
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+        
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
---
+++ cardapio/src/main/java/com/hugo/cardapio/food/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package com.hugo.cardapio.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+    
+}
